4909fb572103dd70cbf4b28970520c7e94ef799a
MLDODSON <mldodson@fullsail.edu>
2020-06-27 17:52:29 -0500

    Variables and Data Types

--- JS_BASICS/basics.js
+++ JS_BASICS/basics.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+var firstName = 'Mike';
+console.log(firstName);
+console.log("My name is " + firstName + ".");
+
+var lastName = 'Johnson';
+var age = 35;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+job = 'Web Designer';
+console.log(firstName + " is a " + job + ".");
